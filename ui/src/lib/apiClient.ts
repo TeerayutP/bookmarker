1e8303c594f8bc4edb7dfe5b87e625faf55caad6
Teerayut <teerayut.p2710@gmail.com>
2026-06-28 22:36:29 +0700

    Revert apiClient 401 interceptor change

--- ui/src/lib/apiClient.ts
+++ ui/src/lib/apiClient.ts
@@ -13,8 +13,7 @@ apiClient.interceptors.request.use((config) => {
 apiClient.interceptors.response.use(
   (res) => res,
   (err) => {
-    const url = err.config?.url ?? ''
-    if (err.response?.status === 401 && !url.startsWith('/auth/')) {
+    if (err.response?.status === 401) {
       localStorage.removeItem('access_token')
       window.location.href = '/login'
     }
